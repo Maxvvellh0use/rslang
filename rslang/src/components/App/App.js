332ed21a042b52a4db4d94321e914106786515b4
Maksim Pavlov <pavlovmark100@gmail.com>
2020-06-18 00:35:09 +0400

    fix: remove .idea

--- rslang/src/components/App/App.js
+++ rslang/src/components/App/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
+import Card from "../Card/Card";
 
 function App() {
+
   return (
-    <div className="card_word">
-      <section className="card_word__main">
-        <p className="card_word__main__sentence">sentence</p>
-        <p className="card_word__main__sentence_translation">sentence_translation</p>
-        <p className="card_word__main__word_translation">word_translation</p>
-      </section>
-    </div>
+    <Card />
   );
 }
 
