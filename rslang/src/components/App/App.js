e0f4ec7aca608321e79867e6fea5a11f018b07c8
Maksim Pavlov <pavlovmark100@gmail.com>
2020-06-18 02:09:56 +0400

    fix: remove useless code

--- rslang/src/components/App/App.js
+++ rslang/src/components/App/App.js
@@ -3,12 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="card_word">
-      <section className="card_word__main">
-        <p className="card_word__main__sentence">sentence</p>
-        <p className="card_word__main__sentence_translation">sentence_translation</p>
-        <p className="card_word__main__word_translation">word_translation</p>
-      </section>
+    <div>
     </div>
   );
 }
