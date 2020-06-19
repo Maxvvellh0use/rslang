5b874952765c31af9b3b7b6189355b8b045bae8b
Maksim Pavlov <pavlovmark100@gmail.com>
2020-06-19 21:17:06 +0400

    feat: add states audio button and create progress bar

---
+++ rslang/src/components/Card/ProgressBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ProgressBar(props) {
+    return (
+        <div className="wrapper progress">
+            <div className="progress_block">
+                <div className="progress_block__number">0</div>
+                <div className="progress_block__bar"></div>
+                <div className="progress_block__number">50</div>
+            </div>
+        </div>
+    );
+}
+
+export default ProgressBar;
