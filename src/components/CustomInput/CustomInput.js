0ab3373d5ef7dfebc48ba68ecf5c022d45530aa4
Maksim Pavlov <pavlovmark100@gmail.com>
2020-06-23 20:43:55 +0400

    feat: create new component CustomInput and add him focus

---
+++ src/components/CustomInput/CustomInput.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './CustomInput.scss'
+
+class CustomInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.textInput = React.createRef();
+        this.focusInput = this.focusInput.bind(this);
+    }
+
+    focusInput = () => {
+        this.textInput.current.focus();
+    }
+
+    render = () => {
+        return (
+            <div>
+                <input data-check={this.props.dataCheck} style={this.props.style}
+                       ref={this.textInput} type="text"/>
+            </div>
+        )
+
+    };
+}
+
+export default CustomInput;
+
