461ed0772f677858e901d29e1a916f161609d1a6
enl001 <enliven001@mail.ru>
2020-06-25 10:16:02 +0300

    feat: add category tab styling

--- src/components/DictionaryCategoryTab/DictionaryCategoryTab.js
+++ src/components/DictionaryCategoryTab/DictionaryCategoryTab.js
@@ -2,47 +2,41 @@ import React from 'react';
 import './DictionaryCategoryTab.scss';
 import cx from 'clsx';
 
-const DEFAULT_CLASS = 'dictionary__tabs__tab';
+const DEFAULT_CLASS = 'dictionary__tab';
 
 export default class DictionaryCategoryTab extends React.Component {
-  
-
   static defaultProps = {
-    className: DEFAULT_CLASS,    
-    selectedClassName: `${DEFAULT_CLASS}_selected`,    
+    className: DEFAULT_CLASS,
+    selectedClassName: `${DEFAULT_CLASS}_selected`,
     selected: false,
-    id: null,   
+    id: null,
     name: 'Tab',
-    counter: 0, 
+    counter: 0,
   };
 
   render() {
-
-    const {      
-      id,      
-      selected,      
+    const {
+      id,
+      selected,
       className,
-      selectedClassName,      
+      selectedClassName,
       name,
-      counter,      
+      counter,
       ...attributes
     } = this.props;
 
     return (
       <div
-        {...attributes}        
+        {...attributes}
         className={cx(className, {
-            [selectedClassName]: selected,            
+          [selectedClassName]: selected,
         })}
-        id={id}        
+        id={id}
       >
-        <h3>{name}</h3>
-        <p>{counter}</p>
-        <p>{selected? 'true': 'false'}</p>
-
+        <h3 className="dictionary__tab__header">{name}</h3>
+        <p className="dictionary__tab__counter">{counter}</p>
+        <p>{selected ? 'true' : 'false'}</p>
       </div>
     );
   }
-
-
 }
--- src/components/DictionaryPage/DictionaryPage.js
+++ src/components/DictionaryPage/DictionaryPage.js
@@ -6,37 +6,37 @@ export default class DictionaryPage extends React.Component {
 
   handleClick(event) {    
     const panel = document.querySelector('.dictionary__panel');
-    const tabs = document.querySelectorAll('.dictionary__tabs__tab');
-    tabs.forEach((tab) => tab.classList.remove('dictionary__tabs__tab_selected'));
+    const tabs = document.querySelectorAll('.dictionary__tab');
+    tabs.forEach((tab) => tab.classList.remove('dictionary__tab_selected'));
     switch (event.target.id) {
       case 'tab-dictionary-learning':
         panel.classList.remove('dictionary__panel_difficult-checked');
         panel.classList.remove('dictionary__panel_removed-checked');
         panel.classList.add('dictionary__panel_learning-checked');
-        document.querySelector('.dictionary__tabs__tab_learning')
-          .classList.add('dictionary__tabs__tab_selected');
+        document.querySelector('.dictionary__tab_learning')
+          .classList.add('dictionary__tab_selected');
           //event.target.setAttribute('SELECTED', true);
         break;
       case 'tab-dictionary-difficult':
         panel.classList.remove('dictionary__panel_removed-checked');
         panel.classList.remove('dictionary__panel_learning-checked');
         panel.classList.add('dictionary__panel_difficult-checked');
-        document.querySelector('.dictionary__tabs__tab_difficult')
-          .classList.add('dictionary__tabs__tab_selected');
+        document.querySelector('.dictionary__tab_difficult')
+          .classList.add('dictionary__tab_selected');
           //event.target.setAttribute('SELECTED', true);
         break;
       case 'tab-dictionary-removed':
         panel.classList.remove('dictionary__panel_learning-checked');
         panel.classList.remove('dictionary__panel_difficult-checked');
         panel.classList.add('dictionary__panel_removed-checked');
-        document.querySelector('.dictionary__tabs__tab_removed')
-          .classList.add('dictionary__tabs__tab_selected');
+        document.querySelector('.dictionary__tab_removed')
+          .classList.add('dictionary__tab_selected');
           //event.target.setAttribute('SELECTED', true);
         break;
       default:
-        panel.classList.remove('dictionary__panel_learning-checked');
-        panel.classList.remove('dictionary__panel_difficult-checked');
-        panel.classList.remove('dictionary__panel_removed-checked');
+        // panel.classList.remove('dictionary__panel_learning-checked');
+        // panel.classList.remove('dictionary__panel_difficult-checked');
+        // panel.classList.remove('dictionary__panel_removed-checked');
         break;
     }
         
@@ -60,7 +60,7 @@ export default class DictionaryPage extends React.Component {
             <div className="dictionary__tabs">             
               
               <DictionaryCategoryTab 
-                className="dictionary__tabs__tab dictionary__tabs__tab_learning"
+                className="dictionary__tab dictionary__tab_learning"
                 id="tab-dictionary-learning"
                 onClick={this.handleClick}
                 name="Learning words"
@@ -68,7 +68,7 @@ export default class DictionaryPage extends React.Component {
                 selected={true}
                 />
               <DictionaryCategoryTab 
-                className="dictionary__tabs__tab dictionary__tabs__tab_difficult"
+                className="dictionary__tab dictionary__tab_difficult"
                 id="tab-dictionary-difficult"
                 onClick={this.handleClick}
                 name="Difficult words"
@@ -76,7 +76,7 @@ export default class DictionaryPage extends React.Component {
                 />
 
               <DictionaryCategoryTab 
-                className="dictionary__tabs__tab dictionary__tabs__tab_removed"
+                className="dictionary__tab dictionary__tab_removed"
                 id="tab-dictionary-removed"
                 onClick={this.handleClick}
                 name="Removed words"
