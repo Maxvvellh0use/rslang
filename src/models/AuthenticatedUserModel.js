be6901e0d199649db01bacc697b76c56cdbf8c26
AlexanderKurtsevich <alexander.kurtsevich@gmail.com>
2020-06-26 21:36:12 +0300

    refactor: rebase again

--- src/models/AuthenticatedUserModel.js
+++
@@ -1,8 +0,0 @@
-import UserModel from './UserModel';
-export default class AuthenticatedUserModel extends UserModel {
-  constructor (email, password, id, token) {
-    super({email, password});
-    this.id = id;
-    this.token = token;
-  }
-}
