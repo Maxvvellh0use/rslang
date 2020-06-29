70aca3bcd533da0f030b4248a24fd4edd7add20e
Alexandra Burdina <sunh2oalex@gmail.com>
2020-06-29 23:42:42 +0300

    fix: fix PR bugs

---
+++ src/components/AuthorizationPage/const.js
@@ -0,0 +1,4 @@
+export const iconPath = '../../assets/img/icons';
+export const emailRegExp = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
+export const passwordRegExp = /^(?=.*[A-Z])(?=.*[!#-&*-.;:_@?[\]{}])(?=.*[0-9])(?=.*[a-z]).{8,}$/;
+
