620367711e87d7a726842f5be1ffa22631a34ac5
Alexandra Burdina <sunh2oalex@gmail.com>
2020-07-10 18:49:01 +0300

    feat: add svg icons

--- src/components/MainPage/WeekProgress/TodayGoal.js
+++ src/components/MainPage/WeekProgress/TodayGoal.js
@@ -1,36 +1,51 @@
 import React, { Component } from 'react';
+import UserSettings from '../../../data/UserSettings';
+import {SVGIcon} from '../WeekProgress/SVGIcon'
 class TodayGoal extends Component {
   state = {
     isDone: false,
-    mountOfDailyCards: 50,
+    wordsPerDay: 50,
     mountOfTodayCards: 15,
   }
-
+  settingsRequest = async () =>{
+    try {
+      let userSettings = await UserSettings.getUserSettings(localStorage.userId);
+      console.log('User settings: ', userSettings);
+    } catch (error) {
+      console.log(error);    
+    }
+  }
   componentWillMount() {
-    let title, text, statistic
+    //this.settingsRequest();
+    let title, text, statistic, wordsPerDay;
     if(this.state.isDone){
       title = 'Цель выполнена'
-      text = 'Завершена серия из '+ this.state.mountOfDailyCards + ' карточек!'
+      text = 'Завершена серия из '+ this.state.wordsPerDay + ' карточек!'
       statistic = 'Отличная работа!'
     } else {
       title = 'Цель на сегодня'
-      text = 'Завершить '+ this.state.mountOfDailyCards + ' карточек'
+      text = 'Завершить '+ this.state.wordsPerDay + ' карточек'
       statistic = 'Сегодня вы выполнили ' + this.state.mountOfTodayCards + 
-                  ' карточек. Для достижения цели завершите ' + this.state.mountOfDailyCards + ' карточек'
+                  ' карточек. Для достижения цели завершите ' + this.state.wordsPerDay + ' карточек'
 
     }
     this.setState({
       title: title,
       text: text,
-      statistic: statistic
+      statistic: statistic,
+      wordsPerDay: wordsPerDay
     });
   }
+
  render () {
    return (
-    <div className='today-goal-block'>
-      <p className='today-goal-title'>{this.state.title}</p>
-      <p className='today-goal-text'>{this.state.text}</p>
-      <p className='today-goal-statistic'>{this.state.statistic}</p>
+    <div className='today-goal-block-text-img'>
+      <SVGIcon width='60px' height='60px' color='red' text='2'/>
+      <div className='today-goal-block-text'>
+        <p className='today-goal-title'>{this.state.title}</p>
+        <p className='today-goal-text'>{this.state.text}</p>
+        <p className='today-goal-statistic'>{this.state.statistic}</p>
+      </div>
     </div>
    )
  }
--- src/components/MainPage/WeekProgress/WeekProgress.js
+++ src/components/MainPage/WeekProgress/WeekProgress.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import '../Progress.scss'
 import TodayGoal from './TodayGoal'
+import {SVGIcon} from '../WeekProgress/SVGIcon'
+
 class WeekProgress extends Component {
   state = {
     
@@ -11,6 +13,16 @@ class WeekProgress extends Component {
        <div className='week-progress-container'>
          <h4 className='today-title'>Еженедельный прогресс</h4>
          <TodayGoal/>
+         <div className='week-block'>
+          <SVGIcon width='40px' height='40px' color='red' text='1'/>
+          <SVGIcon width='40px' height='40px' color='red' text='1'/>
+          <SVGIcon width='40px' height='40px' color='red' text='1'/>
+          <SVGIcon width='40px' height='40px' color='red' text='1'/>
+          <SVGIcon width='40px' height='40px' color='red' text='1'/>
+          <SVGIcon width='40px' height='40px' color='red' text='2'/>
+          <SVGIcon width='40px' height='40px' color='red' text='1'/>
+
+         </div>
        </div>
    )
  }
