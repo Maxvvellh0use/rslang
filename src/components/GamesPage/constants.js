92b75b7cfd7ebbb53332379cd477804d51c1439e
Maksim Pavlov <pavlovmark100@gmail.com>
2020-07-12 18:14:21 +0400

    feat: add link to englishPuzzle

--- src/components/GamesPage/constants.js
+++ src/components/GamesPage/constants.js
@@ -5,9 +5,11 @@ import AGameImage from '../../assets/img/games/AGame.jpg';
 import SPGameImage from '../../assets/img/games/SPGame.png';
 import SAGameImage from '../../assets/img/games/SAGame.jpg'
 
+export const englishPuzzleLink = 'https://rslang-team32-maxvvellh0useenglish.netlify.app/'
+
 const GAMES = [
     {title: 'Speak It', description: 'Игра для тренировки произношения', id: 0, img: SIGameImage},
-    {title: 'English Puzzle', description: 'Собирайте предложения из перемешанных слов', id: 1, img: EPGameImage},
+    {title: 'English Puzzle', description: 'Собирайте предложения из перемешанных слов', id: 1, img: EPGameImage, link: englishPuzzleLink},
     {title: 'Спринт', description: 'За отведенный промежуток времени необходимо указать принадлежат ли переводы словам', id: 2, img: SPGameImage},
     {title: 'Аудиовызов', description: 'Слушайте слово на английском и выбирайте его перевод на русском', id: 3, img: AGameImage, link: '/main/games/audio_call'},
     {title: 'CrossWord', description: 'Угадывайте слова по буквам исходя из заданных вопросов', id: 4, img: CWGameImage},
