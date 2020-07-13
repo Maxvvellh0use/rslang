15901d7491a0d28d08e51c3e41afe3870023aaa3
Alexandra Burdina <sunh2oalex@gmail.com>
2020-07-14 01:38:00 +0300

    feat: add cards markup

--- src/components/AboutTeam/AboutTeamPage.js
+++ src/components/AboutTeam/AboutTeamPage.js
@@ -1,2 +1,18 @@
 import React, { Component } from 'react';
 import './AboutTeam.scss'
+import PersoanlImformationComponent from './PersoanlImformationComponent'
+
+class AboutTeam extends Component{
+    render = () => (
+        <div className='container'>
+            <div className='row'>
+                <PersoanlImformationComponent/>
+                <PersoanlImformationComponent/>
+                <PersoanlImformationComponent/>
+                <PersoanlImformationComponent/>
+                <PersoanlImformationComponent/>
+            </div>
+        </div>
+    )
+}
+export default AboutTeam;
---
+++ src/components/AboutTeam/PersoanlImformationObject.js
@@ -0,0 +1,20 @@
+export const PersonalInformationObject = {
+    photo:  [],
+    name:   ['Burdina Alexandra',
+             'Mikhaduk Nikita',
+             'Burdina Helen',
+             'Titok Maxim',
+             'Vayl Daria'],
+
+    github: ['https://github.com/Alexandra2503',
+             'https://github.com/Nikita3026',
+             'https://github.com/Alexandra2503',
+             'https://github.com/Nikita3026',
+             'https://github.com/Alexandra2503'],
+
+    work:   ['Authorization and registration',
+             'Main page and savanah game',
+             'SpeakIt game',
+             'English-puzzle game',
+             'Statistic'],
+}
