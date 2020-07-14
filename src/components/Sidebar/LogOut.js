e9086ec6eb698831a1feebb07ac7d0e406bf6e51
Andrey Obraztsov <oaoandrew@gmail.com>
2020-07-14 22:41:43 +0300

    feat: add responsive logout

--- src/components/Sidebar/LogOut.js
+++ src/components/Sidebar/LogOut.js
@@ -12,7 +12,7 @@ class LogOut extends React.Component {
         return (
             <div className="logout">
                 <div className="logout_box">
-                    <h2>Вы действительно хотите выйти?</h2>
+                    <div className="logout_title">Вы действительно хотите выйти?</div>
                     <div>
                         <button onClick={this.logout} type="button" className="btn btn-success btn-lg">Да</button>
                         <button type="button" className="btn btn-danger btn-lg" onClick={this.props.history.goBack}>Нет</button>
