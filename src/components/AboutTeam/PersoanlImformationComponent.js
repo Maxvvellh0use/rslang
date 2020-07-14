35ca2f35c0be04fb20b2bac9531621a105eb45b2
Alexandra Burdina <sunh2oalex@gmail.com>
2020-07-15 00:57:36 +0300

    feat: add styles

--- src/components/AboutTeam/PersoanlImformationComponent.js
+++ src/components/AboutTeam/PersoanlImformationComponent.js
@@ -6,14 +6,16 @@ import githubIcon from '../../assets/img/icons/github.png'
 class PersoanlImformationComponent extends Component {
 
     render = () => (
-        <div className='card testimonial-card personal-information-component col-6 col-xs-12'>
+        <div className='card testimonial-card personal-information-component col-6 col-sm-5 col-md-5 col-lg-4 reverse'>
             <img className='developer-photo card-img-top' src={photo1} alt='developer photo'/>
-            <div className='personal-information-text-block card-body'>
+            <div className='personal-information-text-block card-body card-body-cascade'>
                 <p className='developer-name card-title'>
-                    <strong>{PersonalInformationObject.name[1]}</strong>
+                   <strong>{PersonalInformationObject.name[this.props.arrayNumber]}</strong>
                 </p>
-                <p>{PersonalInformationObject.work[1]}</p>
-                <a type="button" className="btn-floating btn-small btn-fb" href={PersonalInformationObject.github[1]}>
+                <p className='persone-description'>{PersonalInformationObject.work[this.props.arrayNumber]}</p>
+                <div className='separator'/>
+                <p className='personal-contribution'>{PersonalInformationObject.work[this.props.arrayNumber]}</p>
+                <a type="button" className="btn-floating btn-small btn-fb" href={PersonalInformationObject.github[this.props.arrayNumber]}>
                     <img className='developer-github-icon' src={githubIcon} alt='github icon'/>                
                 </a>
             </div>
