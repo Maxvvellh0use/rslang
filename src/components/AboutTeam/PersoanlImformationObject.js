af6ce8c7bdfca1bad5ca84a6072ecb3a9d936bf5
Alexandra Burdina <sunh2oalex@gmail.com>
2020-07-15 14:11:49 +0300

    feat: add personal information

--- src/components/AboutTeam/PersoanlImformationObject.js
+++ src/components/AboutTeam/PersoanlImformationObject.js
@@ -1,19 +1,23 @@
 import MaximPhoto from '../../assets/img/developersPhoto/Maxim.png'
-import Vyacheslav from '../../assets/img/developersPhoto/Vyacheslav.jpg'
+import VyacheslavPhoto from '../../assets/img/developersPhoto/Vyacheslav.jpg'
+import AlexandraPhoto from '../../assets/img/developersPhoto/Alexandra.jpg'
+import AlexandrPhoto from '../../assets/img/developersPhoto/Alexandr.jpg'
 
 
 export const PersonalInformationObject = {
-    photo:  [MaximPhoto,
-             Vyacheslav,],
+    photo: [MaximPhoto,
+            VyacheslavPhoto,
+            AlexandrPhoto,
+            AlexandraPhoto],
     name:   ['Максим Павлов',
              'Вячеслав Шаболтиев',
-             '',
+             'Александр Курцевич',
              '',
              'Александра Бурдина'],
 
     github: ['https://github.com/Maxvvellh0use',
              'https://github.com/enl001',
-             '',
+             'https://github.com/alexanderkurtsevich',
              '',
              'https://github.com/Alexandra2503'],
 
@@ -26,8 +30,8 @@ export const PersonalInformationObject = {
 
     description:   
            ['В данном проекте выполнял стартовую страницу и страницу карточек, а также мини-игры english puzzle, Аудиовызов и Саваннa',
-            'В данном проекте выполнял запросы к REST API, а также сделал страницу словаря.',
+            'В данном проекте реализовывал запросы к REST API, а также сделал страницу словаря.',
+            'В данном проекте выполнял страницу с играми, страницу с настройками, а также мини-игры Speak it, CrossWord',
             '',
-            '',
-            'В данном проекте выполняла страницу авторизации и регистрации и страницу "о команде"'],
+            'В данном проекте выполняла страницу авторизации и регистрации, страницу "о команде"'],
 }
