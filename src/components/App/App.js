f0b3353247428b0c4a1730984d20a11b90567ec3
Alexandra Burdina <sunh2oalex@gmail.com>
2020-07-15 21:53:29 +0300

    fix: fix app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.scss';
 import '../../style/index.scss';
 import StartPage from "../StartPage/StartPage";
-import AboutTeam from '../AboutTeam/AboutTeamPage';
 
 function App(props) {
   return (
     <div>
-      <AboutTeam/>
-{/*       <StartPage history={props.history}/> */}
+      <StartPage history={props.history}/>
     </div>
   );
 }
