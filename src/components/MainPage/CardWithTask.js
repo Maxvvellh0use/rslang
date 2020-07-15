009260f8058330fd695e69d1d909edda31dda135
Alexandra Burdina <sunh2oalex@gmail.com>
2020-07-15 20:15:25 +0300

    fix: fix routing

--- src/components/MainPage/CardWithTask.js
+++ src/components/MainPage/CardWithTask.js
@@ -5,27 +5,19 @@ import './MainPage.scss'
 
 const CardWithTask = ({path, image, task, linkMessage, motivationalMessage}) => {
     return (
-/*         <Router>
- */            <div>
-                <div className='card main-page-card'>
-                    <img className='image' src={image} alt='image'/>
-                    <div className='text-block card-body'>
-                        <p className='card-title task-number'>
-                            <strong>{task}</strong>
-                        </p>
-                        <Link to={path} className='linkMessage'>{linkMessage}</Link>
-                        <p className='motivational-message'>{motivationalMessage}</p>
-                    </div>
+        <div>
+            <div className='card main-page-card'>
+                <img className='image' src={image} alt='image'/>
+                <div className='text-block card-body'>
+                    <p className='card-title task-number'>
+                        <strong>{task}</strong>
+                    </p>
+                    <Link to={path} className='linkMessage'>{linkMessage}</Link>
+                    <p className='motivational-message'>{motivationalMessage}</p>
                 </div>
-                {/* <Switch>
-                    <Route path='/main/words'>
-                        <Card history={this.props.history}
-                            switchOverlay={this.switchOverlay}/>
-                    </Route>
-                </Switch> */}
             </div>
-/*         </Router>
- */    )
+        </div>
+    )
 }
 
 export default CardWithTask;
